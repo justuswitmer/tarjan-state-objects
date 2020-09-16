018394c7b6041e5512e6a8a19cdd8246aeae0dfe
Justus Witmer <justus.witmer@gmail.com>
2020-09-16 14:35:59 -0500

    Basic App component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // Defining initial state
+  state = {
+    cohortName: 'Tarjan',
+    salutation: 'Hello',
+    name: 'Justus'
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{this.state.cohortName} Learns State with Objects</h1>
+        <div>
+          {this.state.salutation}! My name is {this.state.name}.
+        </div>
       </div>
     );
   }
